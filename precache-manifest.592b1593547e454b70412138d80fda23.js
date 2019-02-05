05800fc46c6312c7bb651630af7d7e7abb83528a
Yurii <yurii.oleshko@gmail.com>
2019-02-05 19:33:58 +0200

    Updates

---
+++ precache-manifest.592b1593547e454b70412138d80fda23.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "28600053d4609eaa2b32",
+    "url": "/Einshtein_App/static/js/runtime~main.28600053.js"
+  },
+  {
+    "revision": "7ab5f4f5d72170f5076c",
+    "url": "/Einshtein_App/static/js/main.7ab5f4f5.chunk.js"
+  },
+  {
+    "revision": "f4f3ceda2cd3badd5973",
+    "url": "/Einshtein_App/static/js/1.f4f3ceda.chunk.js"
+  },
+  {
+    "revision": "7ab5f4f5d72170f5076c",
+    "url": "/Einshtein_App/static/css/main.274ecc0d.chunk.css"
+  },
+  {
+    "revision": "94349dcfd3cd8350570c6398b4934c17",
+    "url": "/Einshtein_App/index.html"
+  }
+];
